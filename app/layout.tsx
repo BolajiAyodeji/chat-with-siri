31d041a84852f8deaa06c3ce328aa255236fa5bc
Bolaji Ayodeji <sirbeejay1@gmail.com>
2024-02-09 11:52:53 +0100

    feat: add homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     template: "%s | Chat With Siri",
   },
   description:
-    "A simple text-to-speech app using OpenAI's GPT-3 and Eleven Labs' Text-to-Speech API.",
+    "A simple text-to-speech chatbot (OpenAI's GPT-3 and ElevenLabs' API).",
   twitter: {
     card: "summary_large_image",
     creator: "@iambolajiayo",
--- app/page.tsx
+++ app/page.tsx
@@ -2,119 +2,144 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <p className="text-center text-lg border-b border-gray-600 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4">
+        A simple full-stack text-to-speech chatbot built using Nextjs,{" "}
+        <a
+          href="https://platform.openai.com/docs/guides/text-generation/chat-completions-api"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          OpenAI&apos;s GPT-3 Chats Completions API
+        </a>
+        , and{" "}
+        <a
+          href="https://github.com/elevenlabs/elevenlabs-js"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ElevenLabs&apos; Text-to-Speech API
+        </a>
+        .
+      </p>
+
+      <div className="grid gap-6 lg:gap-0 items-center lg:max-w-4xl lg:w-full grid-cols-3">
+        <a
+          href="https://nextjs.org?utm_source=bolajiayodeji.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/next.svg"
+            className="dark:invert"
+            alt="Nextjs's Logo"
+            width={200}
+            height={10}
+            priority
+          />
+        </a>
+        <a
+          href="https://openai.com?utm_source=bolajiayodeji.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/openai.svg"
+            className="dark:invert"
+            alt="OpenAI's Logo"
+            width={200}
+            height={10}
+            priority
+          />
+        </a>
+        <a
+          href="http://elevenlabs.io/?from=bolajiayodeji2995"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/elevenlabs.svg"
+            className="dark:invert"
+            alt="Elevenlabs's Logo"
+            width={200}
+            height={10}
+            priority
+          />
+        </a>
+      </div>
+
+      <div className="mb-40 lg:mb-32 grid lg:grid-cols-3 text-center lg:max-w-5xl lg:w-full lg:text-left">
+        <a
+          href="/chat"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold text-blue-500`}>
+            Chat With Siri{" "}
+            <span className="ml-4 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              ➤
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Ask the chatbot a question by text and get an audio response
+            alongside text.
+          </p>
+        </a>
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Read Tutorial{" "}
+            <span className="ml-4 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              ➤
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn how this demo was built by reading a comprehensive blogpost.
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/BolajiAyodeji/chat-with-siri"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            OSS Code{" "}
+            <span className="ml-4 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              ➤
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the public code of this demo on GitHub and maybe contribute
+            to it.
+          </p>
+        </a>
+      </div>
+
+      <div className="fixed bottom-0 w-full lg:w-2/4 my-8">
+        <hr className="my-4 lg:my-0 border-gray-600 pb-6" />
+        <Image
+          src="/fav.svg"
+          className="mx-auto"
+          alt="Chat With Siri Logo"
+          width={50}
+          height={10}
+        />
+        <p className="text-center">
+          Chat With Siri | &copy;{" "}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://bolajiayodeji.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="text-center py-12 border-b-2 border-white lg:border-0">
-        <span>👁️‍🗨️</span>
-        <p>
-          LLMs are capable of making errors; it&apos;s advisable to verify
-          critical information :).
+            Bolaji Ayodeji
+          </a>{" "}
+          | 2024 - {new Date().getFullYear()}
         </p>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
